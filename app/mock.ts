0c511ce93f3363b0cadd0f8947a166f162c250a0
Maria Tassiana <mariatassiana1@gmail.com>
2022-11-02 17:49:40 -0300

    creating mock

---
+++ app/mock.ts
@@ -0,0 +1,14 @@
+import { product } from './produtos';
+
+export const products: product[] = [
+  { name:'Chair', category: 'House', amount: 50, price: 150, code: 1},
+  { name:'Book', category: 'Reading', amount: 125, price: 30, code: 2},
+  { name:'Pen', category: 'Studying', amount: 145, price: 5, code: 3},
+  { name:'Towel', category: 'House', amount: 58, price: 50, code: 4},
+  { name:'Dress', category: 'Clothing', amount: 26, price: 100, code: 5},
+  { name:'Shoes', category: 'Clothing', amount: 85, price: 200, code: 6},
+  { name:'Table', category: 'House', amount: 45, price: 400, code: 7},
+  { name:'Pencil', category: 'Studying', amount: 56, price: 4, code: 8},
+  { name:'T-shirt', category: 'Clothing', amount: 40, price: 35, code: 9},
+  { name:'Makeup kit', category: 'Dermocosmetic', amount: 39, price: 100, code: 10}
+];
---
+++ app/produtos.ts
@@ -0,0 +1,7 @@
+export interface product {
+    name: string;
+    category: string;
+    amount: number;
+    price: number;
+    code: number;
+  }
